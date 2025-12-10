c8458873c38ee453df6535d607b97367e6fb7326
Anik720 <heanik39@gmail.com>
2025-12-10 19:42:35 +0600

    fixed the user relation with batch

--- src/AcademicFunction/btach/dto/update-batch.dto.ts
+++ src/AcademicFunction/btach/dto/update-batch.dto.ts
@@ -9,18 +9,20 @@ import {
 } from 'class-validator';
 
 export class UpdateBatchDto extends PartialType(CreateBatchDto) {
-  @ValidateIf(o => o.batchStartingDate || o.batchClosingDate)
+  @ValidateIf((o) => o.batchStartingDate || o.batchClosingDate)
   @IsDateString()
   @IsOptional()
   batchStartingDate?: string;
 
-  @ValidateIf(o => o.batchStartingDate || o.batchClosingDate)
+  @ValidateIf((o) => o.batchStartingDate || o.batchClosingDate)
   @IsDateString()
   @IsOptional()
   batchClosingDate?: string;
 
   @IsString()
   @IsOptional()
-  @Matches(/^\d{4}-\d{4}$/, { message: 'Session year must be in format YYYY-YYYY' })
+  @Matches(/^\d{4}-\d{4}$/, {
+    message: 'Session year must be in format YYYY-YYYY',
+  })
   sessionYear?: string;
 }
